95aaa3bb312c27964f1981bc03e3a76c592ec25e
Kristaps Esterlins <kristaps.esterlins@gmail.com>
2014-07-30 23:22:09 +0300

    basic info about mt6592 platform

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,16 +5,17 @@ USE_CAMERA_STUB := true
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
+TARGET_BOARD_PLATFORM := mt6592
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := TRUE
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := lcsh92_wet_jb9
 
-BOARD_KERNEL_CMDLINE := console=ttyMT0,921600n1 vmalloc=496M slub_max_order=0 lcm=1-nt35521_hd720_dsi_vdo_auo fps=6130 bootprof.pl_t=877 bootprof.lk_t=1166 printk.disable_uart=0 boot_reason=4
-BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_BASE := 0x10008000
 BOARD_KERNEL_PAGESIZE := 2048
 
 # fix this up by examining /proc/mtd on a running device
